5bdf18682ad5363142d522db1a8a195c66eea919
Davide-Ficocelli <davide.ficocelli.dev@gmail.com>
2025-03-18 16:19:45 +0100

    event listeners and buttons

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 "use strict";
+
+// Selecting all buttons and elements
+const yellowBtn = document.querySelector(".yellow");
+const blueBtn = document.querySelector(".blue");
+const redBtn = document.querySelector(".red");
+const greenBtn = document.querySelector(".green");
+const resetBtn = document.querySelector(".reset");
+
+const body = document.querySelector("body");
+
+// Functions
+function changeBackgroundColor(color) {
+  body.style.backgroundColor = color;
+}
+
+// Handling click events for each button
+yellowBtn.addEventListener(
+  "click",
+  changeBackgroundColor.bind(changeBackgroundColor, "yellow")
+);
+
+blueBtn.addEventListener(
+  "click",
+  changeBackgroundColor.bind(changeBackgroundColor, "blue")
+);
+
+redBtn.addEventListener(
+  "click",
+  changeBackgroundColor.bind(changeBackgroundColor, "red")
+);
+
+greenBtn.addEventListener(
+  "click",
+  changeBackgroundColor.bind(changeBackgroundColor, "green")
+);
+
+resetBtn.addEventListener(
+  "click",
+  changeBackgroundColor.bind(changeBackgroundColor, "#FFFFFF")
+);
