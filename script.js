5f8cd140017fecc81f5c1c061517e4c142aed3e0
Davide-Ficocelli <davide.ficocelli.dev@gmail.com>
2025-03-19 16:40:22 +0100

    Random color button added

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const blueBtn = document.querySelector(".blue");
 const redBtn = document.querySelector(".red");
 const greenBtn = document.querySelector(".green");
 const resetBtn = document.querySelector(".reset");
+const randomBtn = document.querySelector(".random");
 
 const body = document.querySelector("body");
 const header = document.querySelector("header");
@@ -15,35 +16,39 @@ const header = document.querySelector("header");
 // Changes the background color
 function changeBackgroundColor(color) {
   body.style.backgroundColor = color;
+}
 
-  // Changing header style dynamically
-  backgroundColor === "blue" || backgroundColor === "green"
-    ? (header.style.color = "#FFFFFF")
-    : (header.style.color = "#000000");
+// Creates a random color
+function setRandomColor() {
+  const red = Math.round(Math.random() * 255);
+  const green = Math.round(Math.random() * 255);
+  const blue = Math.round(Math.random() * 255);
+
+  const randomColor = `rgb(${red}, ${green}, ${blue})`;
+  changeBackgroundColor(randomColor);
 }
 
 // Handling click events for each button
 yellowBtn.addEventListener(
   "click",
-  changeBackgroundColor.bind(changeBackgroundColor, "yellow")
+  changeBackgroundColor.bind(null, "rgb(255, 255, 0)")
 );
 
 blueBtn.addEventListener(
   "click",
-  changeBackgroundColor.bind(changeBackgroundColor, "blue")
+  changeBackgroundColor.bind(null, "rgb(0, 0, 255)")
 );
 
 redBtn.addEventListener(
   "click",
-  changeBackgroundColor.bind(changeBackgroundColor, "red")
+  changeBackgroundColor.bind(null, "rgb(255, 0, 0)")
 );
 
 greenBtn.addEventListener(
   "click",
-  changeBackgroundColor.bind(changeBackgroundColor, "green")
+  changeBackgroundColor.bind(null, "rgb(0, 255, 0)")
 );
 
-resetBtn.addEventListener(
-  "click",
-  changeBackgroundColor.bind(changeBackgroundColor, "#FFFFFF")
-);
+resetBtn.addEventListener("click", changeBackgroundColor.bind(null, "#FFFFFF"));
+
+randomBtn.addEventListener("click", setRandomColor.bind(null));
